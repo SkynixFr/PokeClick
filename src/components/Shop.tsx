52506f71bd86c1dbee9480461994e0c52c70647b
SkynixFr <romain.ludovic.day@gmail.com>
2023-12-05 13:30:57 +0100

    feat(success): handling display

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
 
 const Shop = () => {
 	return (
 		<View>
-			<Text>Boutique</Text>
+			<Image source={require('../../assets/snorlax_sleeping.jpeg')} style={{ width: 300, height: 400 }} />
+			<Text>Coming soon...</Text>
 		</View>
 	);
 };
--- src/components/SuccessProvider.tsx
+++ src/components/SuccessProvider.tsx
@@ -10,20 +10,52 @@ export const SuccessProvider = (props: React.PropsWithChildren) => {
 	const dispatch = useDispatch();
 	const successList = useSelector((state: RootState) => state.success.value);
 	const currentLevel = useSelector((state: RootState) => state.level.value);
+	const currentClicks = useSelector((state: RootState) => state.dpc.nbClicks);
 
 	useEffect(() => {
 		function getSuccess() {
 			const successes: SuccessDetails[] = [
 				{
 					id: '1',
-					name: 'Tu cliques ou tu pointes ?',
+					name: 'Gourou de la Gravité',
+					icon: 'sort-amount-up',
 					levels: [10, 50, 100, 500, 1000],
+					lastRewardIndexClaimed: 0,
 					rewards: [
 						{ amount: 10, claimed: false },
 						{ amount: 20, claimed: false },
 						{ amount: 30, claimed: false },
 						{ amount: 40, claimed: false },
 						{ amount: 50, claimed: false }
+					]
+				},
+				{
+					id: '2',
+					name: 'Tyran du Tapotage',
+					icon: 'hand-pointer',
+					levels: [100, 1000, 10000, 100000, 1000000],
+					lastRewardIndexClaimed: 0,
+					rewards: [
+						{
+							amount: 10,
+							claimed: false
+						},
+						{
+							amount: 20,
+							claimed: false
+						},
+						{
+							amount: 30,
+							claimed: false
+						},
+						{
+							amount: 40,
+							claimed: false
+						},
+						{
+							amount: 50,
+							claimed: false
+						}
 					]
 				}
 			];
@@ -35,62 +67,48 @@ export const SuccessProvider = (props: React.PropsWithChildren) => {
 	}, []);
 
 	useEffect(() => {
-		successList.forEach((success: SuccessDetails) => {
-			if (success.levels.includes(currentLevel)) {
+		successList.forEach((success: SuccessDetails, index) => {
+			if (success.id === '1' && success.levels.includes(currentLevel)) {
 				ToastAndroid.show(
 					`Vous avez atteint le niveau ${currentLevel} !`,
 					ToastAndroid.SHORT
 				);
 			}
 
-			success.rewards.forEach(reward => {
-				if (
-					success.levels.includes(currentLevel) &&
-					!reward.claimed &&
-					reward.amount == currentLevel
-				) {
-					ToastAndroid.show(
-						`Récompense de succès disponible pour ${success.name} !`,
-						ToastAndroid.SHORT
-					);
-				}
-			});
+			if (
+				success.id === '1' &&
+				success.levels.includes(currentLevel) &&
+				!success.rewards[index].claimed
+			) {
+				ToastAndroid.show(
+					`Récompense de succès disponible pour ${success.name} !`,
+					ToastAndroid.SHORT
+				);
+			}
 		});
-
-		// successList.forEach(success => {
-		// 	console.log(success);
-
-		// 	if (
-		// 		Array.isArray(success.levels) &&
-		// 		success.levels.some(
-		// 			subLevels =>
-		// 				Array.isArray(subLevels) && subLevels.includes(currentLevel)
-		// 		)
-		// 	) {
-		// 		console.log('success');
-		// 		ToastAndroid.show(
-		// 			`Vous avez atteint le niveau ${currentLevel} !`,
-		// 			ToastAndroid.SHORT
-		// 		);
-		// 	}
-
-		// 	success.rewards?.forEach(reward => {
-		// 		if (
-		// 			success.levels?.some(
-		// 				subLevels =>
-		// 					Array.isArray(subLevels) &&
-		// 					subLevels.includes(currentLevel)
-		// 			) &&
-		// 			!reward.claimed
-		// 		) {
-		// 			ToastAndroid.show(
-		// 				`Récompense de succès disponible pour ${success.name} !`,
-		// 				ToastAndroid.SHORT
-		// 			);
-		// 		}
-		// 	});
-		// });
 	}, [successList, currentLevel]);
+
+	useEffect(() => {
+		successList.forEach((success: SuccessDetails, index) => {
+			if (success.id === '2' && success.levels.includes(currentClicks)) {
+				ToastAndroid.show(
+					`Vous avez fait ${currentClicks} clics, ça clic fort !`,
+					ToastAndroid.SHORT
+				);
+			}
+
+			if (
+				success.id === '2' &&
+				success.levels.includes(currentClicks) &&
+				!success.rewards[index].claimed
+			) {
+				ToastAndroid.show(
+					`Récompense de succès disponible pour ${success.name} !`,
+					ToastAndroid.SHORT
+				);
+			}
+		});
+	}, [successList, currentClicks]);
 
 	return props.children;
 };
