fdbba51eb0b41c2fe522b93288009ba6f0970d45
SkynixFr <romain.ludovic.day@gmail.com>
2023-12-05 13:48:52 +0100

    feat(shop): coming soon...

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -3,13 +3,33 @@ import React from 'react';
 
 const Shop = () => {
 	return (
-		<View>
-			<Image source={require('../../assets/snorlax_sleeping.jpeg')} style={{ width: 300, height: 400 }} />
-			<Text>Coming soon...</Text>
+		<View style={styles.container}>
+			<Image
+				source={require('../../assets/snorlax_sleeping.png')}
+				style={{
+					width: 300,
+					height: 400,
+					position: 'absolute'
+				}}
+			/>
+			<Text style={styles.text}>Coming soon...</Text>
 		</View>
 	);
 };
 
 export default Shop;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		position: 'relative',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	text: {
+		marginTop: 175,
+		fontSize: 20,
+		fontWeight: 'bold',
+		zIndex: 1
+	}
+});
