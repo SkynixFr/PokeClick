d03da8b75d2564660782b2455ec3ae4771fcce2b
Ricky Khao <103840102+Luffysonic@users.noreply.github.com>
2023-12-05 15:36:14 +0100

    feat(layout): suppression de la const Stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ReduxProvider } from '../components/ReduxProvider';
 
 import { db, fireBaseInit } from '../firebase/firebaseInit';
 import { AuthProvider } from '../components/AuthProvider';
 
-const Stack = createNativeStackNavigator();
 export default function RootLayout() {
 	return <RootLayoutNav />;
 }
