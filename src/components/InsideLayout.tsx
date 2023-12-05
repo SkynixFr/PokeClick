a1954c61591028b7bc26d2127609f3551f72ecf6
Ricky Khao <103840102+Luffysonic@users.noreply.github.com>
2023-12-05 14:57:04 +0100

    feat(Layout) : ajout du SuccessProvider

--- src/components/InsideLayout.tsx
+++ src/components/InsideLayout.tsx
@@ -1,17 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Index from '../app/index';
 import { PokemonProvider } from './PokemonProvider';
+import { SuccessProvider } from './SuccessProvider';
 const InsideStack = createNativeStackNavigator();
 export function InsideLayout() {
 	return (
 		<PokemonProvider>
-			<InsideStack.Navigator>
-				<InsideStack.Screen
-					name="index"
-					component={Index}
-					options={{ headerShown: false }}
-				/>
-			</InsideStack.Navigator>
+			<SuccessProvider>
+				<InsideStack.Navigator>
+					<InsideStack.Screen
+						name="index"
+						component={Index}
+						options={{ headerShown: false }}
+					/>
+				</InsideStack.Navigator>
+			</SuccessProvider>
 		</PokemonProvider>
 	);
 }
