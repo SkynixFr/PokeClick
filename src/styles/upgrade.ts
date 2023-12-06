6ecbf6a54acdd732988b7a076e1e86c761c4eb8e
Suranewt <saranyudaorueang@gmail.com>
2023-12-06 16:52:40 +0100

    update(upgrades): update upgrades design

--- src/styles/upgrade.ts
+++ src/styles/upgrade.ts
@@ -3,11 +3,11 @@ const UpgradeStyle = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: 'column',
-		padding: 0.5,
+		padding: 0,
 		borderBottomWidth: 1,
 		borderBottomColor: '#000',
 		// Add the following to make it take up more width
-		width: '90%', // Adjust the percentage as needed
+		width: '100%', // Adjust the percentage as needed
 		marginBottom: 70
 	}
 });
