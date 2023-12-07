3652b47578c218015634d93028c7542fc311891a
Ricky Khao <103840102+Luffysonic@users.noreply.github.com>
2023-12-07 09:05:55 +0100

    feat(ConnectionProvider): Design Page hors connexion

--- src/components/ConnectionProvider.tsx
+++ src/components/ConnectionProvider.tsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, ImageBackground } from 'react-native';
+import {
+	Text,
+	View,
+	Image,
+	ImageBackground,
+	ActivityIndicator
+} from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
 
 import { StyleSheet } from 'react-native';
+
 const ConnectionProvider = (props: React.PropsWithChildren) => {
 	const netInfo = useNetInfo();
 	const [isConnected, setIsConnected] = useState(true);
@@ -26,38 +33,49 @@ const ConnectionProvider = (props: React.PropsWithChildren) => {
 	}, [netInfo]);
 
 	if (!isConnected) {
-		// Display a loading screen or message until the connection is restored
 		return (
 			<View style={NoConnection.noInternetContainer}>
-				<ImageBackground
-					source={require('../../assets/pikachu.gif')}
-					style={NoConnection.image}
-					resizeMode="contain"
-				/>
-				<Text style={NoConnection.noInternetText}>
-					Attente d'une connexion ...{' '}
-				</Text>
+				<View style={NoConnection.overlay}>
+					<ImageBackground
+						source={require('../../assets/pikachu.gif')}
+						style={NoConnection.image}
+						resizeMode="contain"
+					/>
+					<Text style={NoConnection.noInternetText}>
+						Attente d'une connexion ...
+					</Text>
+					<ActivityIndicator size="large" color="#00ff00" />
+				</View>
 			</View>
 		);
 	}
 
 	return props.children;
 };
+
 export const NoConnection = StyleSheet.create({
 	noInternetContainer: {
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
+	overlay: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 20 // Ajustez cette valeur pour définir l'espacement souhaité
+	},
 	image: {
-		width: 250, // Adjust the width of the image as needed
-		height: 250, // Adjust the height of the image as needed
-		marginBottom: 20 // Margin to separate image and text
+		width: 250,
+		height: 250,
+		marginBottom: 10 // Ajustez cette valeur pour définir l'espacement souhaité
 	},
 	noInternetText: {
 		fontSize: 18,
 		fontWeight: 'bold',
-		textAlign: 'center' // Center the text
+		textAlign: 'center',
+		marginTop: 10 // Ajustez cette valeur pour définir l'espacement souhaité
 	}
 });
+
 export default ConnectionProvider;
